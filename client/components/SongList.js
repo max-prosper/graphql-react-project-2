105bd36d6778b2e79c6da65c7b3912b1aee6832c
Max Prosper <maxim.yaaman@gmail.com>
2017-10-12 15:46:01 +0300

    AddSong query implemented with refetching and redirecting back to the list of songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
 	renderSongs() {
 		return this.props.data.songs.map((song, index) => {
-			return <li key={index} className='collection-item'>{song.title}</li>;
+			return (
+				<li key={index} className="collection-item">
+					{song.title}
+				</li>
+			);
 		});
 	}
 
@@ -13,16 +19,15 @@ class SongList extends Component {
 		if (this.props.data.loading) {
 			return <div>LOADING...</div>;
 		}
-		return <ul className='collection'>{this.renderSongs()}</ul>;
+		return (
+			<div>
+				<ul className="collection">{this.renderSongs()}</ul>
+				<Link to="/songs/new" className="btn-floating btn-large red right">
+					<i className="material-icons">add</i>
+				</Link>
+			</div>
+		);
 	}
 }
-
-const query = gql`
-	{
-		songs {
-			title
-		}
-	}
-`;
 
 export default graphql(query)(SongList);
